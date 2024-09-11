03b1b642d42ba930cc49557f8d2829e73094ee3b
T-cordoba <tommy.cormo@gmail.com>
2024-09-11 09:14:52 -0500

    menu inicial

---
+++ calculator.py
@@ -0,0 +1,19 @@
+run=True
+while run == True:
+    print("Bienvenido a la calculadora")
+    print("que operación deseas hacer? selecciona el número de la que deseas")
+    print("")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("5. Salir")
+
+    selection=int(input("..."))
+    a = int(input("Dame el número 1...."))
+    b = int(input("Dame el número 2...."))
+    
+    if selection == 1:
+        resultado= a + b
+        print(f"la respeusta es {resultado}")
+        print("")
